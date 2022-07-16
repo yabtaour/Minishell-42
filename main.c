0439f735d96a0f504e4255be101a4cf990fbcf83
yabtaour <younessabtaouri0@gmail.com>
2022-07-16 15:06:31 +0100

    jj

--- main.c
+++ main.c
@@ -45,8 +45,160 @@ char	*ft_trim(char *str)
 	return (str);
 }
 
-void	ft_delete_quotes(t_data *data)
+int	ft_real_len(char *cmd)
+{
+	int	len = 0;
+	int	flag_d = 0;
+	int	flag_s = 0;
+	int	i = 0;
 
+	while (cmd[i])
+	{
+		if (cmd[i] == '"' && flag_s == 0)
+		{
+			len++;
+			flag_d = ft_change_flag(flag_d);
+		}
+		if (cmd[i] == '\'' && flag_d == 0)
+		{
+			len++;
+			flag_s = ft_change_flag(flag_s);
+		}
+		i++;
+	}
+	return (len);
+}
+
+int	*ft_get_index(char *cmd, int type)
+{
+	int		i = 0;
+	char	c;
+	int		len = 0;
+	int	flag_d = 0;
+	int	flag_s = 0;
+	int		*index;
+
+	if (type == 1)
+	{
+		while (cmd[i])
+		{
+			if (cmd[i] == '"' && flag_s == 0)
+			{
+				len++;
+				flag_d = ft_change_flag(flag_d);
+			}
+			if (cmd[i] == '\'' && flag_d == 0)
+			{
+				len++;
+				flag_s = ft_change_flag(flag_s);
+			}
+			i++;			
+		}
+		index = malloc (sizeof(int) * len);
+		len = 0;
+		i = 0;
+		while (cmd[i])
+		{
+			if (cmd[i] == '"' && flag_s == 0)
+			{
+				index[len] = i;
+				len++;
+				flag_d = ft_change_flag(flag_d);
+			}
+			if (cmd[i] == '\'' && flag_d == 0)
+			{
+				index[len] = i;
+				len++;
+				flag_s = ft_change_flag(flag_s);
+			}
+			i++;		
+		}
+	}
+	return (index);
+}
+
+int	ft_exist(int i, int *index, int len)
+{
+	int	idx = 0;
+
+	while (idx < len)
+	{
+		if (index[idx] == i)
+			return (1);
+		idx++;
+	}
+	return (0);
+}
+
+char	*ft_delete(char *cmd)
+{
+	int		*index;
+	int		i = 0;
+	int		len = 0;
+	int		r = 0;
+	char	*new_cmd;
+
+	index = ft_get_index(cmd, 1);
+	r = ft_real_len(cmd);
+	new_cmd = malloc (strlen(cmd) + 1 - r);
+	while (cmd[i])
+	{
+		if (!ft_exist(i, index, r))
+		{
+			new_cmd[len] = cmd[i];
+			len++;
+		}
+		i++;
+	}
+	new_cmd[len] = '\0';
+	free(index);
+	printf("%s\n", new_cmd);
+	return (new_cmd);
+}
+
+int	ft_exist_quotes(char *cmd)
+{
+	int	i = 0;
+
+	while (cmd[i])
+	{
+		if (cmd[i] == '"' || cmd[i] == '\'')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+void	ft_delete_quotes(t_data *data)
+{
+	t_cmd	*cmd_clone;
+	int		r_len = 0;
+	char	*temp = NULL;
+	int		i = 0;
+	int		j = 0;
+
+	cmd_clone = data->lst_cmd;
+	while (cmd_clone)
+	{
+			while (cmd_clone->cmd[i])
+			{
+				if (ft_exist_quotes(cmd_clone->cmd[i]))
+				{
+					HERE
+					temp = ft_substr(cmd_clone->cmd[i], 0, strlen(cmd_clone->cmd[i]));
+					HERE
+					free(cmd_clone->cmd[i]);
+					HERE
+					printf("%s\n", ft_delete(cmd_clone->cmd[i]));
+					// cmd_clone->cmd[i] = strcpy(cmd_clone->cmd[i], ft_delete(cmd_clone->cmd[i]));
+					HERE
+					printf("%s\n", cmd_clone->cmd[i]);
+					i++;
+				}
+			}
+		cmd_clone = cmd_clone->next;
+	}
+}
 
 int	main(int argc, char **argv, char **env)
 {
@@ -77,27 +229,28 @@ int	main(int argc, char **argv, char **env)
 				ft_free_lexer(data.lst_lexer);
 				continue;
 			}
-			data.error = ft_syntax_analyzer(&data);
-			if (data.error)
-				continue;
+			HERE
 			ft_expanding(&data);
+			HERE
 			ft_print_lexer(data.lst_lexer);
+			HERE
 			ft_parsing(&data);
+			HERE
 			ft_print_lexer(data.lst_lexer);
-			ft_delete_quotes(&data);
+			HERE
+			// ft_delete_quotes(&data);
+			HERE
 			ft_print_cmd(data.lst_cmd);
-			// if (data.lst_cmd)
-				// echo(&data, data.lst_cmd, data.lst_cmd->fd_in);
-			// cd(&data, data.lst_cmd);
-			// pwd(&data, data.lst_cmd, 1);
-			// ft_print_lexer(data.lst_lexer);
-			// comment this if you want to edit the parsing to avoid segfaults
-			// also you can run single cmnts right now
-			// unset(&data, "zzz");
-			// printf("%d\n", export(&data, data.lst_cmd, 1));
-			// exe(&data);
+			if (data.lst_cmd)
+				echo(&data, data.lst_cmd, data.lst_cmd->fd_in);
+			cd(&data, data.lst_cmd);
+			pwd(&data, data.lst_cmd, 1);
+			ft_print_lexer(data.lst_lexer);
+			unset(&data, "zzz");
+			printf("%d\n", export(&data, data.lst_cmd, 1));
+			exe(&data);
 			ft_free_lexer(data.lst_lexer);
-			// ft_free_cmd(data.lst_cmd);
+			ft_free_cmd(data.lst_cmd);
 		}
 	}
 	return (0);
--- parsing/parsing_utils.c
+++ parsing/parsing_utils.c
@@ -26,48 +26,6 @@ char	*ft_substr(char *s, int start, size_t len)
 	return (ptr);
 }
 
-static size_t	word_count(char *s, char c)
-{
-	int	i;
-	int	count;
-	char quotes;
-
-	i = 0;
-	count = 0;
-	while (s[i] != '\0')
-	{
-		if (s[i] == '"' || s[i] == '\'')
-		{
-			quotes = s[i];
-			i++;
-			while (quotes != s[i])
-				i++;
-			count++;
-		}
-		i++;
-		while (s[i] != '\0' && s[i] == c)
-			i++;
-		if (s[i] != '\0')
-		{
-			if ((s[i - 1] == c || i == 0))
-				count++;
-			i++;
-		}
-	}
-	return (count);
-}
-
-static char	**ft_free_tab(char **str, int i)
-{
-	while (i >= 0)
-	{
-		free(str[i]);
-		i--;
-	}
-	free(str);
-	return (str);
-}
-
 void	free_split(char **arguments)
 {
 	int	i;
@@ -81,15 +39,6 @@ void	free_split(char **arguments)
 	free(arguments);
 }
 
-static char	**ft_allocation(char *s, char c)
-{
-	char	**ptr;
-
-	if (s == NULL)
-		return (NULL);
-	ptr = calloc((word_count(s, c) + 2), sizeof(char *));
-	return (ptr);
-}
 
 char	*ft_join_args(char *s1, char *s2)
 {
@@ -101,43 +50,6 @@ char	*ft_join_args(char *s1, char *s2)
 	return (result);
 }
 
-char	**ft_split(char *s, char c)
-{
-	char	**ptr;
-	int		j;
-	int		i;
-	int		k = 0;
-	char	quotes;
-
-	ptr = ft_allocation(s, c);
-	if (!ptr)
-		return (ptr);
-	i = 0;
-	while (*s)
-	{
-		j = 0;
-		while (*s && *s == c)
-			s++;
-		if (*s == '"' || *s == '\'')
-		{
-			quotes = *s;
-			while (s[++k] != quotes)
-				j++;
-			j++;
-		}
-		while (s[j] != '\0' && s[j] != c)
-			j++;
-		if (j != 0)
-		{
-			ptr[i] = ft_substr(s, 0, j);
-			if (ptr[i] == NULL)
-				return (ft_free_tab(ptr, i));
-			i++;
-		}
-		s = s + j;
-	}
-	return (ptr);
-}
 
 char	*ft_strjoin(char *s1, char *s2)
 {
@@ -166,4 +78,75 @@ char	*ft_strjoin(char *s1, char *s2)
 	str[strlen(s1) + strlen(s2)] = '\0';
 	free(s1);
 	return (str);
+}
+
+static size_t	word_count(const char *s, char c)
+{
+	int	i;
+	int	count;
+
+	i = 0;
+	count = 0;
+	while (s[i] != '\0')
+	{
+		while (s[i] != '\0' && s[i] == c)
+			i++;
+		if (s[i] != '\0')
+		{
+			if ((s[i - 1] == c || i == 0))
+				count++;
+			i++;
+		}
+	}
+	return (count);
+}
+
+static char	**ft_free_tab(char	**str, int i)
+{
+	while (i >= 0)
+	{
+		free(str[i]);
+		i--;
+	}
+	free(str);
+	return (str);
+}
+
+static char	**ft_allocation(char const *s, char c)
+{
+	char	**ptr;
+
+	if (s == NULL)
+		return (NULL);
+	ptr = calloc((word_count(s, c) + 1), sizeof(char *));
+	return (ptr);
+}
+
+char	**ft_split(char *s, char c)
+{
+	char	**ptr;
+	int		j;
+	int		i;
+
+	ptr = ft_allocation(s, c);
+	if (!ptr)
+		return (ptr);
+	i = 0;
+	while (*s)
+	{
+		while (*s && *s == c)
+			s++;
+		j = 0;
+		while (s[j] != '\0' && s[j] != c)
+			j++;
+		if (j != 0)
+		{
+			ptr[i] = ft_substr(s, 0, j);
+			if (ptr[i] == NULL)
+				return (ft_free_tab(ptr, i));
+			i++;
+		}
+		s = s + j;
+	}
+	return (ptr);
 }
