b94578af8778ba7fd3f401d9560f4dcff6495f7a
yabtaour <younessabtaouri0@gmail.com>
2022-07-23 13:04:19 +0100

    done with env norme

--- env/ft_env.c
+++ env/ft_env.c
@@ -1,5 +1,14 @@
 #include "../minishell.h"
 
+void	ft_create_env_list(t_env **envi, char **env)
+{
+	int	i;
+
+	i = 0;
+	while (env[i])
+		*envi = ft_add_to_env_back(*envi, env[i++]);
+}
+
 void	ft_env(t_data *data)
 {
 	ft_create_env_list(&data->lst_env, data->env);
--- env/ft_env_list.c
+++ env/ft_env_list.c
@@ -1,43 +1,23 @@
 #include "../minishell.h"
 
-//---------create envirenment node--------//
-
 t_env	*ft_new_env_node(char *value)
 {
 	t_env	*node;
-	int		i = 0;
-	int		j = 0;
-	char	*name;
+	int		i;
 
+	i = 0;
 	node = NULL;
-	if (!value)
-		return (NULL);
 	node = malloc(sizeof(t_env));
 	if (!node)
 		return (NULL);
 	while (value[i] != '=')
 		i++;
-	node->name = malloc (i + 1);
-	i = -1;
-	while (value[++i] != '=')
-		node->name[i] = value[i];
-	node->name[i] = '\0';
-	i = strlen(value) - j;
-	node->value = malloc(i * sizeof(char));
-	i = -1;
-	j = 0;
-	while (value[j] != '=')
-		j++;
-	j++;
-	while (value[j])
-		node->value[++i] = value[j++];
-	node->value[++i] = '\0';
+	node->name = ft_substr(value, 0, i);
+	node->value = ft_substr(value, i + 1, ft_strlen(value) - i);
 	node->next = NULL;
 	node->prev = NULL;
 	return (node);
 }
-
-//--------add node to the back of envirenment list--------//
 
 t_env	*ft_add_to_env_back(t_env *envi, char *value)
 {
@@ -55,15 +35,4 @@ t_env	*ft_add_to_env_back(t_env *envi, char *value)
 	envi_clone->next = node;
 	node->prev = envi_clone;
 	return (envi);
-}
-
-//------create envirenment list--------//
-
-void	ft_create_env_list(t_env **envi, char **env)
-{
-	int	i;
-
-	i = 0;
-	while (env[i])
-		*envi = ft_add_to_env_back(*envi, env[i++]);
 }
