6c96f8723cd034f8fa9e49d960133ccdbb13b1f6
Amine Reda saf <redasm39@gmail.com>
2022-07-25 16:57:34 +0100

    update herdoc

--- execution/herdoc.c
+++ execution/herdoc.c
@@ -4,13 +4,13 @@
 // in bash herdoc do not output to a file just created
 // the herdoc before the syntax error
 
-int	her_finished(t_data *data, t_cmd *cmd_clone, int **pip)
+int	her_finished(t_data *data, t_cmd *cmd_clone, int **pip, int i)
 {
 	close(cmd_clone->fd_in);
 	close(cmd_clone->her_in);
 	close_fds(cmd_clone);
 	close_pipes(pip, data->general.lent);
-	exit(data->error);
+	exit(i);
 }
 
 void	print_for_her(t_cmd *cmd_clone, char *buff)
@@ -19,31 +19,34 @@ void	print_for_her(t_cmd *cmd_clone, char *buff)
 	ft_putstr_fd("\n", cmd_clone->her_in);
 }
 
-int	ft_herdoc(t_data *data, t_cmd *cmd_lst, int **pip)
+int	ft_herdoc(t_data *data, t_cmd *cmd_lst, int **pip, int i)
 {
+	int		status;
 	int		pid;
-	int		i;
 	char	*buff;
 	t_cmd	*cmd_clone;
+	int		idx = 0;
 
 	pid = -99;
-	i = 0;
 	cmd_clone = cmd_lst;
 	pid = fork();
-	if (data->her_doc && pid == 0)
+	if (cmd_clone->her_doc_num && pid == 0)
 	{
-		while (1 && i < cmd_clone->her_doc_num)
+		while (1 && idx < cmd_lst->her_doc_num)
 		{
 			buff = readline("heredoc> ");
 			if (!buff)
 				return (1);
 			else if (buff[0] != '\0' && !strcmp(buff, data->eof[i]))
+			{
 				i++;
-			else if (data->eof[i + 1] == NULL)
+				idx++;
+			}
+			else
 				print_for_her(cmd_clone, buff);
 		}
-		her_finished(data, cmd_clone, pip);
+		her_finished(data, cmd_clone, pip, i);
 	}
-	waitpid(pid, 0, 0);
+	waitpid(pid, &status, 0);
 	return (1);
 }
--- execution/start_execution.c
+++ execution/start_execution.c
@@ -45,6 +45,7 @@ int	start_execution(t_data *data, int **pip, int idx)
 		idx = ft_fork(data, idx);
 		if (data->general.pid == 0 && cmd_clone->fd_in != -69)
 		{
+			rl_catch_signals = 1;
 			ft_dup(data->general.lent, cmd_clone, pip);
 			close_pipes(pip, data->general.lent);
 			data->error = ft_if_builtin(data, cmd_clone);
@@ -52,6 +53,7 @@ int	start_execution(t_data *data, int **pip, int idx)
 				cmd_path = ft_cmd_exist(data, cmd_clone, 0);
 			if (cmd_path)
 				ft_execute_cmd(data, cmd_path, cmd_clone);
+			rl_on_new_line();
 			exit(1);
 		}
 		cmd_clone = cmd_clone->next;
